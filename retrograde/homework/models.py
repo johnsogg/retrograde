862e7038154015585a74b86dfeed2325c46d207a
Gabe Johnson <gabe.johnson@gmail.com>
2013-02-10 18:04:53 -0700

    Added grading system. Need to fix database on RG.cs

--- retrograde/homework/models.py
+++ retrograde/homework/models.py
@@ -131,4 +131,4 @@ class ExamResult(models.Model):
     ta = models.ForeignKey(TeachingAssistant, null=True)
 
     def __unicode__(self):
-        return self.student.first_name + " " + self.student.last_name + "<" + self.student.get_profile().cu_id + ">: " + str(score) + " points"
+        return self.student.first_name + " " + self.student.last_name + "<" + self.student.get_profile().cu_id + ">: " + str(self.score) + " points"
