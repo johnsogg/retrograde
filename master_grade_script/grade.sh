7ddf37c5c4da2e919707eb58f8c8b9c11375c7fb
Gabe Johnson <gabe.johnson@gmail.com>
2013-04-07 18:54:32 -0600

    Added slightly improved grade.sh script

---
+++ master_grade_script/grade.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+INS_DIR=/Users/johnsogg/Projects/grading-scripts
+ASSIGNMENT=hw_8_pointers
+STUD_FILE=/Users/johnsogg/Projects/cs1300/homeworks/hw-8-pointers/cpp/pointers.cpp
+PYTHONPATH=/Users/johnsogg/Projects/retrograde/retrograde ./grade.py $INS_DIR $ASSIGNMENT joe@colorado.edu $STUD_FILE
