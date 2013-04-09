d622c0f59d1fd2d5e5f422f26bf19c27f145b39c
Gabe Johnson <gabe.johnson@gmail.com>
2013-04-09 00:09:42 -0600

    CS1300 linked list grading stuff

--- master_grade_script/grade.sh
+++ master_grade_script/grade.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 INS_DIR=/Users/johnsogg/Projects/grading-scripts
-ASSIGNMENT=hw_8_pointers
-STUD_FILE=/Users/johnsogg/Projects/cs1300/homeworks/hw-8-pointers/cpp/pointers.cpp
+ASSIGNMENT=hw_9_linked_lists
+STUD_FILE=/Users/johnsogg/Projects/private/hw-9-linked-lists/linked_list.cpp
 PYTHONPATH=/Users/johnsogg/Projects/retrograde/retrograde ./grade.py $INS_DIR $ASSIGNMENT joe@colorado.edu $STUD_FILE
