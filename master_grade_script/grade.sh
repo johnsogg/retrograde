4ade1f4f2f66e91e52f9574fb6a2b02ad88ae90e
Gabe Johnson <gabe.johnson@gmail.com>
2013-04-19 00:50:12 -0600

    Simple change on my disk

--- master_grade_script/grade.sh
+++ master_grade_script/grade.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 INS_DIR=/Users/johnsogg/Projects/grading-scripts
-ASSIGNMENT=hw_9_linked_lists
-STUD_FILE=/Users/johnsogg/Projects/private/hw-9-linked-lists/linked_list.cpp
+ASSIGNMENT=hw_10_fsm
+STUD_FILE=/Users/johnsogg/Projects/private/hw-10-fsm/fsm.cpp
 PYTHONPATH=/Users/johnsogg/Projects/retrograde/retrograde ./grade.py $INS_DIR $ASSIGNMENT joe@colorado.edu $STUD_FILE
