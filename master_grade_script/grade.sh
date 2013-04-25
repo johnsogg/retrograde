7996654ed3ab9cbe8c35025e64886b47874ec1a4
Gabe Johnson <gabe.johnson@gmail.com>
2013-04-25 12:16:33 -0600

    using cs 1300 objects hw

--- master_grade_script/grade.sh
+++ master_grade_script/grade.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 INS_DIR=/Users/johnsogg/Projects/grading-scripts
-ASSIGNMENT=hw_10_fsm
-STUD_FILE=/Users/johnsogg/Projects/private/hw-10-fsm/fsm.cpp
+ASSIGNMENT=hw_10_objects
+STUD_FILE=/Users/johnsogg/Projects/cs1300/homeworks/hw-10-objects/cpp/linked_list.cpp
 PYTHONPATH=/Users/johnsogg/Projects/retrograde/retrograde ./grade.py $INS_DIR $ASSIGNMENT joe@colorado.edu $STUD_FILE
